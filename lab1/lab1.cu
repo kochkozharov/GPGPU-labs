67c184780c726b8e0ba9779d36bbdec1998a7668
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2025-10-05 21:29:51 +0000

    lab1

--- lab1/lab1.cu
+++ lab1/lab1.cu
@@ -1,18 +1,62 @@
 #include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <iomanip>
 
-// This function (kernel) will be executed on the GPU
-__global__ void cuda_hello_kernel() {
-    printf("Hello World from GPU!\n");
+#define CSC(call)  									                \
+do {											                    \
+    cudaError_t res = call;							                \
+    if (res != cudaSuccess) {							            \
+        fprintf(stderr, "ERROR in %s:%d. Message: %s\n",			\
+                __FILE__, __LINE__, cudaGetErrorString(res));		\
+        exit(0);								                    \
+    }										                        \
+} while(0)
+
+
+__global__ void vecAddKernel(const double* a, const double* b, double* c, int n) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) c[i] = a[i] + b[i];
 }
 
 int main() {
-    // Launch the kernel on the GPU
-    // <<<grid_size, block_size>>> specifies the execution configuration
-    // Here, 1 block and 1 thread per block are launched
-    cuda_hello_kernel<<<1, 5>>>();
+    int n;
+    std::cin >> n;
+    std::vector<double> h_a(n), h_b(n);
+    for (size_t i = 0; i < n; ++i) {
+        std::cin >> h_a[i];
+    }
+    for (size_t i = 0; i < n; ++i) {
+        std::cin >> h_b[i];
+    }
 
-    // Wait for the GPU to finish its work before the CPU continues
-    cudaDeviceSynchronize();
+
+    double *d_a = nullptr, *d_b = nullptr, *d_c = nullptr;
+    size_t bytes = sizeof(double) * (size_t)n;
+    CSC(cudaMalloc(&d_a, bytes));
+    CSC(cudaMalloc(&d_b, bytes));
+    CSC(cudaMalloc(&d_c, bytes));
+
+    CSC(cudaMemcpy(d_a, h_a.data(), bytes, cudaMemcpyHostToDevice));
+    CSC(cudaMemcpy(d_b, h_b.data(), bytes, cudaMemcpyHostToDevice));
+
+    const int blockSize = 256;
+    int gridSize = static_cast<int>((n + blockSize - 1) / blockSize);
+    vecAddKernel<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);
+    CSC(cudaGetLastError());
+    CSC(cudaDeviceSynchronize());
+
+    std::vector<double> h_c(n);
+    CSC(cudaMemcpy(h_c.data(), d_c, bytes, cudaMemcpyDeviceToHost));
+
+    CSC(cudaFree(d_a));
+    CSC(cudaFree(d_b));
+    CSC(cudaFree(d_c));
+
+    std::cout << std::setprecision(10) << std::fixed;
+    for (size_t i = 0; i < n; ++i) {
+        std::cout << h_c[i] << '\n';
+    }
 
     return 0;
 }
