3ffe97c7b8a669beec6fd8a5811eee4fb8ca67de
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2025-10-06 11:01:23 +0000

    Fix dependence of threads num on input size

--- lab1/lab1.cu
+++ lab1/lab1.cu
@@ -1,25 +1,36 @@
-#include <stdio.h>
+#include <cuda_runtime.h>
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <iomanip>
 
-#define CSC(call)  									                \
-do {											                    \
-    cudaError_t res = call;							                \
-    if (res != cudaSuccess) {							            \
-        fprintf(stderr, "ERROR in %s:%d. Message: %s\n",			\
-                __FILE__, __LINE__, cudaGetErrorString(res));		\
-        exit(0);								                    \
-    }										                        \
+#define CSC(call)                                           \
+do {                                                        \
+    cudaError_t res = (call);                               \
+    if (res != cudaSuccess) {                               \
+        fprintf(stderr, "CUDA ERROR in %s:%d : %s\n",       \
+                __FILE__, __LINE__, cudaGetErrorString(res));\
+        exit(EXIT_FAILURE);                                 \
+    }                                                       \
 } while(0)
 
+constexpr int BLOCKS_NUM  = 128;
+constexpr int THREADS_NUM = 256;
 
-__global__ void vecAddKernel(const double* a, const double* b, double* c, int n) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) c[i] = a[i] + b[i];
+__global__ void vecComponentWiseMultKernel(const double* a, const double* b, double* c, int n) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for (int i = tid; i < n; i += stride) {
+        c[i] = a[i] * b[i];
+    }
 }
 
 int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
     int n;
     std::cin >> n;
     std::vector<double> h_a(n), h_b(n);
@@ -32,7 +43,7 @@ int main() {
 
 
     double *d_a = nullptr, *d_b = nullptr, *d_c = nullptr;
-    size_t bytes = sizeof(double) * (size_t)n;
+    size_t bytes = sizeof(double) * static_cast<size_t>(n);
     CSC(cudaMalloc(&d_a, bytes));
     CSC(cudaMalloc(&d_b, bytes));
     CSC(cudaMalloc(&d_c, bytes));
@@ -40,9 +51,7 @@ int main() {
     CSC(cudaMemcpy(d_a, h_a.data(), bytes, cudaMemcpyHostToDevice));
     CSC(cudaMemcpy(d_b, h_b.data(), bytes, cudaMemcpyHostToDevice));
 
-    const int blockSize = 256;
-    int gridSize = static_cast<int>((n + blockSize - 1) / blockSize);
-    vecAddKernel<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);
+    vecComponentWiseMultKernel<<<BLOCKS_NUM, THREADS_NUM>>>(d_a, d_b, d_c, n);
     CSC(cudaGetLastError());
     CSC(cudaDeviceSynchronize());
 
@@ -54,9 +63,11 @@ int main() {
     CSC(cudaFree(d_c));
 
     std::cout << std::scientific << std::setprecision(10);
-    for (size_t i = 0; i < n; ++i) {
-        std::cout << h_c[i] << ' ';
+    for (int i = 0; i < n; ++i) {
+        if (i) std::cout << ' ';
+        std::cout << h_c[i];
     }
+    std::cout << '\n';
 
     return 0;
 }
