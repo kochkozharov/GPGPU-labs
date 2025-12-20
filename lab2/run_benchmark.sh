0d8c957f8775554254aeed9abcf9157e695da48f
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2025-12-20 12:27:43 +0000

    fix bench

--- lab2/run_benchmark.sh
+++ lab2/run_benchmark.sh
@@ -3,23 +3,23 @@
 # Script to run benchmarks on different image sizes
 
 echo "Generating test images..."
-./target/generate_test_images 32 test_small.data
-./target/generate_test_images 512 test_medium.data
-./target/generate_test_images 2048 test_large.data
+./target/generate_test_images 256 test_small.data
+./target/generate_test_images 1024 test_medium.data
+./target/generate_test_images 4096 test_large.data
 
 echo ""
 echo "Running benchmarks..."
 echo ""
 
-echo "=== Small image (32x32) ==="
+echo "=== Small image (256x256) ==="
 ./target/benchmark test_small.data
 echo ""
 
-echo "=== Medium image (512x512) ==="
+echo "=== Medium image (1024x1024) ==="
 ./target/benchmark test_medium.data
 echo ""
 
-echo "=== Large image (2048x2048) ==="
+echo "=== Large image (4096x4096) ==="
 ./target/benchmark test_large.data
 
 rm test_small.data test_medium.data test_large.data
